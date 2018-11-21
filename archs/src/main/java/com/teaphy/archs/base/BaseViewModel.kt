01c9c06120008b57d3713281ed9699d59933ea5d
tianye <01437772>
2018-11-21 13:33:32 +0800

    1. 优化BaseViewModel

--- archs/src/main/java/com/teaphy/archs/base/BaseViewModel.kt
+++ archs/src/main/java/com/teaphy/archs/base/BaseViewModel.kt
@@ -10,63 +10,67 @@ import io.reactivex.disposables.Disposable
 
 open class BaseViewModel : ViewModel(), Observable {
 
-	private val callbacks: PropertyChangeRegistry = PropertyChangeRegistry()
+    private val callbacks: PropertyChangeRegistry = PropertyChangeRegistry()
 
-	private var mCompositeDisposable: CompositeDisposable = CompositeDisposable()
-	private lateinit var loadingLiveData: MutableLiveData<Boolean>
+    private var mCompositeDisposable: CompositeDisposable = CompositeDisposable()
+    private lateinit var loadingLiveData: MutableLiveData<Boolean>
 
-	override fun onCleared() {
-		super.onCleared()
+    override fun onCleared() {
+        super.onCleared()
 
-		if (!mCompositeDisposable.isDisposed) {
-			mCompositeDisposable.dispose()
-		}
-	}
+        if (!mCompositeDisposable.isDisposed) {
+            mCompositeDisposable.dispose()
+        }
+    }
 
-	fun addDisposable(disposable: Disposable) {
-		mCompositeDisposable.add(disposable)
-	}
+    fun addDisposable(disposable: Disposable) {
+        mCompositeDisposable.add(disposable)
+    }
 
-	/**
-	  * 用来控制是否显示Loading
-	  * @author teaphy
-	  * @date 2018/7/23 上午10:46
-	  */
-	fun updateLoadingStatus(isShow: Boolean) {
-		loadingLiveData.postValue(isShow)
-	}
+    fun addDisposable(operate: () -> Disposable) {
+        mCompositeDisposable.add(operate.invoke())
+    }
 
-	fun getLoadingLiveData(): LiveData<Boolean> {
-		loadingLiveData = MutableLiveData()
-		return loadingLiveData
-	}
+    /**
+     * 用来控制是否显示Loading
+     * @author teaphy
+     * @date 2018/7/23 上午10:46
+     */
+    fun updateLoadingStatus(isShow: Boolean) {
+        loadingLiveData.postValue(isShow)
+    }
 
-	// DataBinding
-	override fun addOnPropertyChangedCallback(
-			callback: Observable.OnPropertyChangedCallback) {
-		callbacks.add(callback)
-	}
+    fun getLoadingLiveData(): LiveData<Boolean> {
+        loadingLiveData = MutableLiveData()
+        return loadingLiveData
+    }
 
-	override fun removeOnPropertyChangedCallback(
-			callback: Observable.OnPropertyChangedCallback) {
-		callbacks.remove(callback)
-	}
+    // DataBinding
+    override fun addOnPropertyChangedCallback(
+            callback: Observable.OnPropertyChangedCallback) {
+        callbacks.add(callback)
+    }
 
-	/**
-	 * Notifies observers that all properties of this instance have changed.
-	 */
-	fun notifyChange() {
-		callbacks.notifyCallbacks(this, 0, null)
-	}
+    override fun removeOnPropertyChangedCallback(
+            callback: Observable.OnPropertyChangedCallback) {
+        callbacks.remove(callback)
+    }
 
-	/**
-	 * Notifies observers that a specific property has changed. The getter for the
-	 * property that changes should be marked with the @Bindable annotation to
-	 * generate a field in the BR class to be used as the fieldId parameter.
-	 *
-	 * @param fieldId The generated BR id for the Bindable field.
-	 */
-	fun notifyPropertyChanged(fieldId: Int) {
-		callbacks.notifyCallbacks(this, fieldId, null)
-	}
+    /**
+     * Notifies observers that all properties of this instance have changed.
+     */
+    fun notifyChange() {
+        callbacks.notifyCallbacks(this, 0, null)
+    }
+
+    /**
+     * Notifies observers that a specific property has changed. The getter for the
+     * property that changes should be marked with the @Bindable annotation to
+     * generate a field in the BR class to be used as the fieldId parameter.
+     *
+     * @param fieldId The generated BR id for the Bindable field.
+     */
+    fun notifyPropertyChanged(fieldId: Int) {
+        callbacks.notifyCallbacks(this, fieldId, null)
+    }
 }
